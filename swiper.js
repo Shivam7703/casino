1ecca24a3fb5061c4c700acac6007c3ac65e0bd8
shivam7703 <shivamg7703@gmail.com>
2025-04-04 16:03:54 +0530

    new

---
+++ swiper.js
@@ -0,0 +1,73 @@
+
+const swiper2 = new Swiper('.swiper2', {
+    loop: true,
+  autoplay:{
+    delay:"3000"
+  }  ,
+  
+  breakpoints: {
+    200: {
+      slidesPerView: 1, 
+      spaceBetween: 15,
+    },
+    530: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    760: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    1050: {
+      slidesPerView: 4,
+      spaceBetween: 25,
+    },
+  },
+  
+  pagination: {
+      el: '.swiper-pagination2',
+      clickable: true,
+    },
+  
+    navigation: {
+        nextEl: '#slide-prev2',
+        prevEl: '#slide-next2',
+    },
+  
+  });
+
+
+
+  const swiper4 = new Swiper('.swiper4', {
+    loop: true,
+  autoplay:{
+    delay:"3500"
+  }  ,
+  
+  breakpoints: {
+    200: {
+      slidesPerView: 1, 
+      spaceBetween: 15,
+    },
+    630: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+  
+    950: {
+      slidesPerView: 3,
+      spaceBetween: 35,
+    },
+  },
+  
+  pagination: {
+      el: '.swiper-pagination4',
+      clickable: true,
+    },
+  
+    navigation: {
+        nextEl: '#slide-prev4',
+        prevEl: '#slide-next4',
+    },
+  
+  });
